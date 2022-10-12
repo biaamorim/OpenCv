286fa836759c1b912f903d411fb61979f836134b
biaamorim <bnaca1@aluno.ifal.edu.br>
2022-10-11 23:41:55 -0300

    realização da questão 2

--- atv1-q1.py
+++ atv1-q1.py
@@ -1,15 +1,19 @@
 import cv2 as cv
 import numpy as np
 
-img = cv.imread('./horaDeAventura.jpeg')
+img = cv.imread('horaDeAventura.jpeg')
 hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
 
+#cores
 blue=[(71 , 107 , 78),(117, 255, 211)]
 red =[(0, 50, 50), (10, 255, 255)]
-green=[(36,25,25),(86, 255,255)]
-yellow=(58, 100, 84)
-yellow1=(41, 100,45)
+green=(50, 180,60)
+green1=(130, 200,89)
+yellow=(9, 220, 204)
+yellow1=(110, 400, 250)
 
+
+#azul
 blue_mask = cv.inRange(hsv, np.array(blue[0]), np.array(blue[1]))
 blue_contours = cv.findContours(blue_mask, cv.RETR_EXTERNAL, cv.FONT_HERSHEY_COMPLEX)[0]
 blue_contours = sorted(blue_contours, key=cv.contourArea, reverse=True)[:2]
@@ -18,43 +22,45 @@ blue_contours = sorted(blue_contours, key=cv.contourArea, reverse=True)[:2]
 if blue_contours: 
     for contour in blue_contours:
         (cx, cy), radius = cv.minEnclosingCircle(contour)
-        cv.putText(img, "Azul", (int(cx) + 50, int(cy)), cv.FONT_HERSHEY_COMPLEX, 1, (194, 100, 78), 2)
+        cv.putText(img, "Azul", (int(cx) + 50, int(cy)), cv.FONT_HERSHEY_COMPLEX, 1, (0,0,0), 2)
         cv.circle(img, (int(cx), int(cy)), int(radius), (194, 100, 78), 3)
         
 
-
+#vermelho
 red_mask = cv.inRange(hsv, np.array(red[0]), np.array(red[1]))
 red_contours = cv.findContours(red_mask, cv.RETR_EXTERNAL, cv.FONT_HERSHEY_COMPLEX)[0]
-red_contours = sorted(red_contours, key=cv.contourArea, reverse=True)[:2]
+red_contours = sorted(red_contours, key=cv.contourArea, reverse=True)[:4]
 
 if red_contours:
     for contour in red_contours:
         (cx, cy), radius = cv.minEnclosingCircle(contour)
-        cv.putText(img, "Vermelho", (int(cx), int(cy)), cv.FONT_HERSHEY_COMPLEX, 0.9, (360, 100, 100), 1)
-        cv.circle(img, (int(cx), int(cy)), int(radius), (360, 100, 95), 6)
+        cv.putText(img, "Vermelho", (int(cx), int(cy)), cv.FONT_HERSHEY_COMPLEX, 0.9, (50, 50, 255), 1)
+        cv.circle(img, (int(cx), int(cy)), int(20),(50, 50, 255), 6)
 
 
-
-green_mask = cv.inRange(hsv, np.array(green[0]), np.array(green[1]))
+#verde
+green_mask = cv.inRange(hsv, green, green1)
 green_contours = cv.findContours(green_mask, cv.RETR_EXTERNAL, cv.FONT_HERSHEY_COMPLEX)[0]
-green_contours = sorted(green_contours, key=cv.contourArea, reverse=True)[:1]
+green_contours = sorted(green_contours, key=cv.contourArea, reverse=True)[:2]
 
 
-if green_contours: 
+if green_contours[:2]: 
     for contour in green_contours:
         (cx, cy), radius = cv.minEnclosingCircle(contour)
-        cv.putText(img, "Verde", (int(cx), int(cy)), cv.FONT_HERSHEY_COMPLEX, 1, (86, 255,255), 2)
-        cv.circle(img, (int(cx), int(cy)), int(radius), (86, 255,255), 3)
+        cv.putText(img, "Verde", (int(cx), int(cy)), cv.FONT_HERSHEY_COMPLEX, 1, (60, 180,60), 2)
+        cv.circle(img, (int(cx), int(cy)), int(20), (60, 180,60), 3)
 
+#amarelo
 yellow_mask = cv.inRange(hsv, yellow, yellow1)
 yellow_contours = cv.findContours(yellow_mask, cv.RETR_EXTERNAL, cv.FONT_HERSHEY_COMPLEX)[0]
-yellow_contours = sorted(yellow_contours, key=cv.contourArea, reverse=True)[:3]
+yellow_contours = sorted(yellow_contours, key=cv.contourArea, reverse=True)[:2]
 
 
 if yellow_contours: 
     for contour in yellow_contours:
         (cx, cy), radius = cv.minEnclosingCircle(contour)
-        cv.putText(img, "Amarelo", (int(cx), int(cy)), cv.FONT_HERSHEY_COMPLEX, 1, (86, 255,255), 2)
+        cv.putText(img, "Amarelo", (int(cx), int(cy)), cv.FONT_HERSHEY_COMPLEX, 1, (5, 300, 24), 2)
         cv.circle(img, (int(cx), int(cy)), int(radius), (39,85,99), 3)
+
 cv.imshow("Color Detected", img)
 cv.waitKey(0)
--- atv1-q2.py
+++ atv1-q2.py
@@ -1,6 +1,45 @@
 import numpy as np
-import cv2 
-import argparse
+import cv2
 
-cap = cv2.VideoCapture("video.mp4")
+cap = cv2.VideoCapture("danca.mp4")
+
+frame_width =int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+frame_heigth = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+fourcc= cv2.VideoWriter_fourcc('X', 'Y', 'I', 'D')
+out =cv2.VideoWriter("output.avi", fourcc, 5.0, (1280, 720))
+ret, frame1= cap.read()
+ret, frame2= cap.read()
+
+while cap.isOpened():
+    diff = cv2.absdiff(frame1,frame2)
+    color = cv2.cvtColor(diff,cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(color,(5,5),0)
+
+    _,thresh =cv2.threshold(blur,20,255,cv2.THRESH_BINARY)
+    
+    dilated = cv2.dilate(thresh,None,iterations=3)
+
+    contours,_ = cv2.findContours( dilated,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in contours:
+        (x,y,w,h) = cv2.boundingRect(contour)
+
+        if cv2.contourArea(contour) < 900:
+            continue
+        cv2.rectangle(frame1,(x,y),(x+w,y+h),(322,13,93),2)
+        cv2.putText(frame1, ("Doguinho dancando com o menino"),(10,20),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,0,0),2)
+    
+
+    img =cv2.resize(frame1,(1280,720))
+
+    out.write(img)
+    
+    cv2.imshow("Video",frame1)
+    frame1 =frame2
+    ret , frame2 = cap.read()
+
+    if cv2.waitKey(40) == 27:
+        break
+
+cv2.destroyAllWindows()
 
